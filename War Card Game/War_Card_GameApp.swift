49690595f9484fa3f9107c59c97ac5b7d9096081
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-06 16:22:04 +0530

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by I O N Groups Pvt Ltd on 2025-05-06.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
