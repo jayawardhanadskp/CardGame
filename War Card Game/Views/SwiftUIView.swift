f482bad1f942ebd382281e6e1ec6a6fad8dbc871
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-09 02:37:40 +0530

    view

--- War Card Game/Views/SwiftUIView.swift
+++ War Card Game/Views/SwiftUIView.swift
@@ -7,12 +7,63 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct ContentView: View {
+    @StateObject var viewModel = GameViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image(viewModel.playerCardImage)
+                    Spacer()
+                    Image(viewModel.cpuCardImage)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    viewModel.deal()
+                }) {
+                    Image("button")
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                        Text("\(viewModel.game.playerScore)")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                        Text("\(viewModel.game.cpuScore)")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                
+                Spacer()
+            }
+        }
     }
 }
 
+
 #Preview {
-    SwiftUIView()
+    ContentView()
 }
