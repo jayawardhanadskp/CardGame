60a54f492110b355f99239e28461fadf3ae08bfe
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-09 02:37:27 +0530

    model

---
+++ War Card Game/Models/CardGame.swift
@@ -0,0 +1,13 @@
+//
+//  CardGame.swift
+//  War Card Game
+//
+//  Created by I O N Groups Pvt Ltd on 2025-05-09.
+//
+
+struct CardGame {
+    var playerCard: Int = 2
+    var cpuCard: Int = 2
+    var playerScore: Int = 0
+    var cpuScore: Int = 0
+}
